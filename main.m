60e74da27671880cd2a090c3d09843fa8f33381c
weirenhuan <530354532@qq.com>
2015-08-12 09:01:55 +0800

    修改master main函数

--- main.m
+++ main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        nslog 321
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
